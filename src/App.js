5a87934d390cf0d6d3280247fd2ad89bd28a4aa2
Amanda <iulianoamanda@gmail.com>
2024-10-28 11:05:09 -0500

    added footer with github link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 import React from "react";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by Amanda Altobelli and is
+        <a href="https://github.com/aaltobelli1/react-project" target="_blank">
+          {" "}
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
