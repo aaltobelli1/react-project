d06468785b7a2dc4840b2a7cf703b0fe4e023424
Amanda <iulianoamanda@gmail.com>
2024-10-28 11:46:15 -0500

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import React from "react";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by Amanda Altobelli and is
-        <a href="https://github.com/aaltobelli1/react-project" target="_blank">
-          {" "}
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        Weather App
+        <footer>
+          This project was coded by Amanda Altobelli and is
+          <a
+            href="https://github.com/aaltobelli1/react-project"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
