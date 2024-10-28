282460350bf2ad1f6b38e69235de5a7f854844f3
Amanda <iulianoamanda@gmail.com>
2024-10-28 12:43:22 -0500

    styled weather css

--- src/Weather.js
+++ src/Weather.js
@@ -4,25 +4,37 @@ export default function Weather() {
   return (
     <div className="Weather">
       <form>
-        <input
-          type="search"
-          placeholder="Enter a city"
-          className="form-control"
-        />
-        <input type="submit" value="Search" className="btn-btn-primary" />
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city"
+              className="form-control"
+              autoFocus="on"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn-btn-primary w-100"
+            />
+          </div>
+        </div>
       </form>
       <h1>Chicago</h1>
       <ul>
         <li>Monday 12:00 </li>
         <li>Partly Cloudy</li>
       </ul>
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-6">
           <img
             src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
             alt="Partly cloudy"
           />
-          60° F
+          <span className="temperature">60</span>
+          <span className="unit">° F</span>
         </div>
         <div className="col-6">
           <ul>
