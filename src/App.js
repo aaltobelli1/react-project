1daad8dd874746ce3648b8df42573d2680eca0e2
Amanda <iulianoamanda@gmail.com>
2024-10-29 13:14:48 -0500

    added API

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
     <div className="App">
       <div className="container">
         <h1> Weather App</h1>
-        <Weather />
+        <Weather defaultCity="Chicago" />
         <footer>
           This project was coded by Amanda Altobelli and is
           <a
