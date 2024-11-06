cc7cb1b01b74d7736fff30dd95f4f6c334a62828
Amanda <iulianoamanda@gmail.com>
2024-11-06 12:21:33 -0600

    finishing touches

--- src/Weather.js
+++ src/Weather.js
@@ -59,7 +59,7 @@ export default function Weather(props) {
           </div>
         </form>
         <WeatherInfo data={weatherData} />
-        <WeatherForecast coordinates={weatherData} />
+        <WeatherForecast coordinates={weatherData.coordinates} />
       </div>
     );
   } else {
